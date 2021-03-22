276e084590fc08fc90668ea0f35b673505595a0e
Jochen Kemnade <jk@sernet.de>
2021-03-22 08:31:09 +0100

    remove unused dependency

--- src/main/java/org/veo/reporting/VeoClientImpl.java
+++ src/main/java/org/veo/reporting/VeoClientImpl.java
@@ -21,9 +21,9 @@ import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequest;
--- src/test/groovy/org/veo/reporting/controllers/ReportControllerSpec.groovy
+++ src/test/groovy/org/veo/reporting/controllers/ReportControllerSpec.groovy
@@ -15,10 +15,10 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 package org.veo.reporting.controllers
-import org.apache.http.HttpHeaders
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.test.web.servlet.MockMvc
