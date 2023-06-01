c8ebfea69103495e4d8bc0434ebdd63f3d751008
Jochen Kemnade <jk@sernet.de>
2023-06-01 09:36:08 +0200

    fix deprecation warnings
    VEO-1116

--- src/main/java/org/veo/reporting/security/ReportingSecurityConfig.java
+++ src/main/java/org/veo/reporting/security/ReportingSecurityConfig.java
@@ -27,7 +27,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -49,10 +51,16 @@ public class ReportingSecurityConfig {
   private String[] allowedHeaders;
 
   @Bean
-  @SuppressFBWarnings("SECSPRCSRFPD")
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable();
-    http.cors();
+    http.csrf(
+        new Customizer<CsrfConfigurer<HttpSecurity>>() {
+          @Override
+          @SuppressFBWarnings("SPRING_CSRF_PROTECTION_DISABLED")
+          public void customize(CsrfConfigurer<HttpSecurity> csrf) {
+            csrf.disable();
+          }
+        });
+    http.cors(Customizer.withDefaults());
     // Anonymous access (a user with role "ROLE_ANONYMOUS" must be
     // enabled for
     // swagger-ui. We cannot disable it.
@@ -60,7 +68,7 @@ public class ReportingSecurityConfig {
     // anonymous user!
     // .anonymous()
     // .disable()
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
   }
 
